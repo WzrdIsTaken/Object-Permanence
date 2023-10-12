dd23f8c2285d49d6c89525b7df6a3ad54add0a42
WzrdIsTaken <62243693+WzrdIsTaken@users.noreply.github.com>
2023-10-12 10:35:19 +0100

    Added assets and amended gitignore

--- ObjectPermanence/main.cpp
+++ ObjectPermanence/main.cpp
@@ -24,7 +24,7 @@
 s32 CALLBACK wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ s32 nShowCmd)
 {
     nabi::NabiCoreSettings nabiCoreSettings = nabi::c_NabiCoreDefaultSettings;
-    nabiCoreSettings.m_DataSettings.m_RouteDocument = "Game\\Data\\ECS\\game_route.xml";
+    nabiCoreSettings.m_DataSettings.m_RouteDocument = "Game\\Data\\ECS\\route.xml";
     nabiCoreSettings.m_DataSettings.m_NabiCoreParseDocuments = nabi::DataSettings::NabiCoreParseMode::All;
 
     nabi::NabiMainParams const nabiParams =
