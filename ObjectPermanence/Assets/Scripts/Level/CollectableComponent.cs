838553e89d8e5a81e8cadc93ff50fc033fd8b304
WzrdIsTaken <62243693+WzrdIsTaken@users.noreply.github.com>
2023-11-09 18:54:29 +0000

    Various VR Stuff

--- ObjectPermanence/Assets/Scripts/Level/CollectableComponent.cs
+++ ObjectPermanence/Assets/Scripts/Level/CollectableComponent.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace ObjectPermanence
@@ -10,19 +11,40 @@ namespace ObjectPermanence
         public CollectableComponent()
         {
         }
-
         void OnTriggerEnter(Collider other)
         {
+            // TODO comment this out when we are just using VR
+
             if (other.CompareTag(Tags.PlayerTag))
             {
                 Collect();
             }
         }
 
-        private void Collect()
+        public void Collect()
         {
             DebugManager.Instance.Log(LogLevel.Info, DebugCategory.Level, "Player collected a collectable");
-            FindFirstObjectByType<CollectableTrackerComponent>().Collect();
+            FindFirstObjectByType<CollectableTrackerComponent>()?.Collect();
+
+            StartCoroutine(FadeOut());
+        }
+
+        private IEnumerator FadeOut()
+        {
+            Renderer renderer = GetComponent<Renderer>();
+            Color initialColor = renderer.material.color;
+            Color targetColor = new Color(initialColor.r, initialColor.g, initialColor.b, 0.0f);
+
+            const float fadeDuration = 1.0f;
+            float elapsedTime = 0f;
+
+            while (elapsedTime < fadeDuration)
+            {
+                elapsedTime += Time.deltaTime;
+                renderer.material.color = Color.Lerp(initialColor, targetColor, elapsedTime / fadeDuration);
+                yield return null;
+            }
+
             Destroy(gameObject);
         }
     }
--- ObjectPermanence/Assets/Scripts/Player/ActivateTeleportationRayComponent.cs
+++ ObjectPermanence/Assets/Scripts/Player/ActivateTeleportationRayComponent.cs
@@ -10,10 +10,12 @@ namespace ObjectPermanence
     {
         [SerializeField] GameObject _teleportationRay;
         [SerializeField] InputActionProperty _activateRay;
+        //[SerializeField] InputActionProperty _cancel;
 
         void Update()
         {
-            _teleportationRay.SetActive(_activateRay.action.ReadValue<float>() > 0.1f);
+            // If problem see https://youtu.be/0xt6dACM_1I?list=PLpEoiloH-4eP-OKItF8XNJ8y8e1asOJud&t=763
+            _teleportationRay.SetActive(/*_cancel.action.ReadValue<float>() == 0.0f &&*/_activateRay.action.ReadValue<float>() > 0.1f);
         }
     }
 }
